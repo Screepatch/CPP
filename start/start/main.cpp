0f6cd8a8173dd882020c5e6aa99ad28cf8faca76
Viacheslav <v.haliullin@pixonic.com>
2019-12-06 01:42:39 +0300

    Start the project in this point

---
+++ start/start/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  start
+//
+//  Created by Вячеслав Халиуллин on 06.12.2019.
+//  Copyright © 2019 Viacheslav Khaliullin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
